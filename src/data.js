b34e598d6e8abc473d6216d7408a200291ebeb43
kuasar <anudar666@gmail.com>
2022-04-06 19:02:01 +0800

    Initial commit for frontend

---
+++ src/data.js
@@ -0,0 +1,84 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://picsum.photos/200/400",
+        title: "SUMMER SALE",
+        desc: "Don't miss the summer sale!",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://picsum.photos/200/400",
+        title: "FALL SALE",
+        desc: "Don't miss the fall sale!",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img: "https://picsum.photos/200/400",
+        title: "TOP SALE",
+        desc: "Don't miss the top sale!",
+        bg: "fbf0f4"
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://picsum.photos/150/150",
+        title: "SHIRT"
+    },
+    {
+        id: 2,
+        img: "https://picsum.photos/150/150",
+        title: "PANTS"
+    },
+    {
+        id: 3,
+        img: "https://picsum.photos/150/150",
+        title: "CAPS"
+    }
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://picsum.photos/150/150"
+    },
+    {
+        id: 2,
+        img: "https://picsum.photos/150/150"
+    },
+    {
+        id: 3,
+        img: "https://picsum.photos/40/40"
+    },
+    {
+        id: 4,
+        img: "https://picsum.photos/40/40"
+    },
+    {
+        id: 5,
+        img: "https://picsum.photos/40/40"
+    },
+    {
+        id: 6,
+        img: "https://picsum.photos/40/40"
+    },
+    {
+        id: 7,
+        img: "https://picsum.photos/40/40"
+    },
+    {
+        id: 8,
+        img: "https://picsum.photos/40/40"
+    },
+    {
+        id: 9,
+        img: "https://picsum.photos/40/40"
+    },
+    {
+        id: 10,
+        img: "https://picsum.photos/40/40"
+    }
+]
